0169f16061843c7a196b20d97df2c24640de3120
jessica23ti <jessica23ti@mahasiswa.pcr.ac.id>
2025-12-10 10:16:14 +0700

    Jes-C7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,10 +61,12 @@ dependencies {
     testImplementation ("junit:junit:4.13.2")
     androidTestImplementation ("androidx.test.ext:junit:1.1.5")
     androidTestImplementation ("androidx.test.espresso:espresso-core:3.5.1")
-
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation ("com.github.bumptech.glide:glide:4.15.1")
+    annotationProcessor ("com.github.bumptech.glide:compiler:4.15.1")
     // Import the Firebase BoM
     implementation(platform("com.google.firebase:firebase-bom:34.6.0"))
-
     // TODO: Add the dependencies for Firebase products you want to use
     // When using the BoM, don't specify versions in Firebase dependencies
     implementation("com.google.firebase:firebase-analytics")
---
+++ app/src/main/java/com/example/sawit/YoutubeResponse.kt
@@ -0,0 +1,27 @@
+package com.example.sawit
+
+data class YoutubeResponse(
+    val items: List<YoutubeItem>
+)
+
+data class YoutubeItem(
+    val snippet: Snippet
+)
+
+data class Snippet(
+    val title: String,
+    val thumbnails: Thumbnails,
+    val resourceId: ResourceId
+)
+
+data class Thumbnails(
+    val medium: Thumbnail
+)
+
+data class Thumbnail(
+    val url: String
+)
+
+data class ResourceId(
+    val videoId: String
+)
