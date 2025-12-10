b50238a9cf1e50f31770fb6464050727a0493c95
jessica23ti <jessica23ti@mahasiswa.pcr.ac.id>
2025-12-10 10:25:58 +0700

    Jes-C8

--- app/src/main/java/com/example/sawit/YoutubeApi.kt
+++ app/src/main/java/com/example/sawit/YoutubeApi.kt
@@ -1,14 +1,26 @@
-package com.example.sawit
-
+import com.example.sawit.YoutubeResponse
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
 
-interface YoutubeApi {
+interface YouTubeApiService {
+
     @GET("playlistItems")
-    suspend fun getPlaylistItems(
+    suspend fun getPlaylistVideos(
         @Query("part") part: String = "snippet",
+        @Query("maxResults") max: Int = 10,
         @Query("playlistId") playlistId: String,
-        @Query("maxResults") max: Int = 20,
         @Query("key") apiKey: String
     ): YoutubeResponse
+}
+
+object RetrofitClient {
+    val instance: YouTubeApiService by lazy {
+        Retrofit.Builder()
+            .baseUrl("https://www.googleapis.com/youtube/v3/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(YouTubeApiService::class.java)
+    }
 }
